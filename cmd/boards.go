afd48d35be06b574247aaf92e648b350a045b78b
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2021-05-06 22:43:34 +0200

    Remove card listing from boards

--- cmd/boards.go
+++ cmd/boards.go
@@ -27,21 +27,10 @@ func boards(args []string) {
 
 	}
 	src = filepath.Clean(src) + "/"
-	cards := cardcabinet.ReadCards(src)
+	
 	boards := cardcabinet.ReadBoards(src)
 	for _, board := range boards {
 		fmt.Println(board.Name)
 
-		fmt.Println()
-
-		fmt.Printf("%+v\n", board)
-		for _, deck := range board.Decks {
-			fmt.Println(deck.Name)
-			fmt.Println("----")
-			fmt.Println()
-			for _, card := range deck.Get(cards) {
-				listCard(card, config)
-			}
-		}
 	}
 }
